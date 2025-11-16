8be6c9532bad4c76715aa0c02266f2e48e97bea0
lidm0707 <lightdemon17@gmail.com>
2025-11-17 00:12:50 +0700

    update call

--- call-program/src/main.rs
+++ call-program/src/main.rs
@@ -2,20 +2,15 @@ use anchor_client::solana_sdk::signer::Signer;
 use anchor_client::solana_sdk::{pubkey::Pubkey, signature::read_keypair_file};
 use anchor_client::{Client, Cluster};
 use anyhow::Result;
+use quest;
+use quest::accounts as quest_accounts;
+use quest::instruction as quest_instruction;
+use quest::state::quest_info::QuestInfo;
+use reward::accounts as reward_accounts;
+use reward::instruction as reward_instruction;
+use reward::state::reward::Reward;
 use std::rc::Rc;
 use std::{env, path::PathBuf};
-
-// --- achieve_game ---
-use achieve_game::Progress;
-use achieve_game::accounts as game_accounts;
-use achieve_game::id;
-use achieve_game::instruction as game_ix;
-
-// --- reward ---
-use reward_achie::Reward;
-use reward_achie::accounts as reward_accounts;
-use reward_achie::id as reward_pid;
-use reward_achie::instruction as reward_ix;
 
 fn main() -> Result<()> {
     use std::time::{SystemTime, UNIX_EPOCH};
@@ -36,44 +31,43 @@ fn main() -> Result<()> {
     let home = env::var("HOME").expect("Cannot find HOME environment variable");
     let keypair_path: PathBuf = [home.as_str(), ".config/solana/id.json"].iter().collect();
     let payer = read_keypair_file(&keypair_path).expect("Failed to read keypair");
-    let pub_key = payer.pubkey();
+    let admin_pub_key = payer.pubkey();
 
     // client
     let client = Client::new(url, Rc::new(payer));
 
     // program clients
-    let ach_prog = client.program(id())?;
-    let reward_prog = client.program(reward_pid())?;
-    println!("admin {:?}", pub_key);
+    let quest = client.program(quest::id())?;
+    let reward = client.program(reward::id())?;
+    println!("admin {:?}", admin_pub_key);
 
     // IDs
-    let game_id: u64 = 43;
+    let quest_id: u64 = 43;
+    let user_id: u64 = 1;
     let server_id: u64 = 123;
     let provider_id: u64 = 456;
-    let event_id: u64 = 123;
     let max_score: u64 = 2;
 
     // --- derive reward PDA ---
-    let (reward_pda, _bump) = Pubkey::find_program_address(
-        &[b"reward", pub_key.as_ref(), &event_id.to_le_bytes()],
-        &reward_prog.id(),
-    );
+    // // admin_pub_key.as_ref(),
+    let (reward_pda, _bump) =
+        Pubkey::find_program_address(&[b"reward", &quest_id.to_le_bytes()], &reward.id());
 
     // --- check reward account ---
-    let reward_account_result = reward_prog.account::<Reward>(reward_pda);
+    let reward_account_result = reward.account::<Reward>(reward_pda);
 
     if reward_account_result.is_err() {
         println!("Reward PDA ไม่พบ, กำลัง initialize...");
 
-        let tx_reward = reward_prog
+        let tx_reward = reward
             .request()
-            .accounts(reward_accounts::Initialize {
-                authority: pub_key,
+            .accounts(reward_accounts::CreateReward {
                 reward: reward_pda,
+                server_admin: admin_pub_key,
                 system_program: anchor_lang::solana_program::system_program::id(),
             })
-            .args(reward_ix::Initialize {
-                event_id,
+            .args(reward_instruction::CreateReward {
+                quest_id,
                 amount: 100,
             })
             .send()?;
@@ -84,42 +78,54 @@ fn main() -> Result<()> {
     }
 
     // --- derive game PDA ---
-    let (game_pda, _bump) = Pubkey::find_program_address(
+    //             b"quest",
+    // server_admin.key().as_ref(),
+    // &quest_id.to_le_bytes().as_ref(),
+    // &user_id.to_le_bytes().as_ref(),
+    // &server_id.to_le_bytes().as_ref(),
+    // &provider_id.to_le_bytes().as_ref(),
+    let (quest_pda, _bump) = Pubkey::find_program_address(
         &[
-            b"game",
-            pub_key.as_ref(),
-            &game_id.to_le_bytes(),
-            &server_id.to_le_bytes(),
-            &provider_id.to_le_bytes(),
-            &event_id.to_le_bytes(),
+            b"quest",
+            admin_pub_key.as_ref(),
+            &quest_id.to_le_bytes().as_ref(),
+            &user_id.to_le_bytes().as_ref(),
+            &server_id.to_le_bytes().as_ref(),
+            &provider_id.to_le_bytes().as_ref(),
         ],
-        &ach_prog.id(),
+        &quest.id(),
     );
-    println!("Derived game PDA = {}", game_pda);
+    println!("Derived game PDA = {}", quest_pda);
 
     // --- check game account ---
-    let game_account_result = ach_prog.account::<Progress>(game_pda);
+    let quest_account_result = quest.account::<QuestInfo>(quest_pda);
 
-    if game_account_result.is_err() {
+    if quest_account_result.is_err() {
         println!("Game PDA ไม่พบ, กำลัง initialize...");
 
-        let tx_init = ach_prog
+        let tx_init = quest
             .request()
-            .accounts(game_accounts::Initialize {
-                game: game_pda,
-                admin: pub_key,
+            .accounts(quest_accounts::CreateQuest {
+                quest_score: quest_pda,
+                server_admin: admin_pub_key,
                 system_program: anchor_lang::solana_program::system_program::id(),
             })
-            .args(game_ix::Initialize {
-                game_id,
+            .args(quest_instruction::CreateQuest {
+                quest_id,
+                user_id,
                 server_id,
                 provider_id,
                 deadline,
-                event_id,
                 max_score,
             })
             .send()?;
-
+        // ctx: Context<CreateQuest>,
+        // quest_id: u64,
+        // user_id: u64,
+        // server_id: u64,
+        // provider_id: u64,
+        // deadline: i64,
+        // max_score: u64,
         println!("InitializeGame tx signature: {}", tx_init);
     } else {
         println!("Game PDA มีอยู่แล้ว");
@@ -127,53 +133,71 @@ fn main() -> Result<()> {
 
     // --- loop ongoing ---
     for i in 0..3 {
-        let tx = ach_prog
+        let tx = quest
             .request()
-            .accounts(game_accounts::OnGoing {
-                game: game_pda,
-                admin: pub_key,
-                reward: reward_pda,
-                reward_program: reward_prog.id(),
+            .accounts(quest_accounts::MakeQuest {
+                quest_score: quest_pda,
+                server_admin: admin_pub_key,
+                system_program: anchor_lang::solana_program::system_program::id(),
             })
-            .args(game_ix::Ongoing { event_id })
+            .args(quest_instruction::MakeQuest {
+                quest_id,
+                user_id,
+                server_id,
+                provider_id,
+            })
             .send()?;
 
         println!(" - Loop {}/3 Ongoing tx signature: {}", (i + 1), tx);
     }
 
     // --- fetch game account ---
-    let game_account: Progress = ach_prog.account(game_pda)?;
+    let quest_account: QuestInfo = quest.account(quest_pda)?;
     println!(
         "Game data - game_id: {}, score: {}, deadline: {}, max_score: {}",
-        game_account.game_id, game_account.score, game_account.deadline, game_account.max_score
+        quest_account.quest_id,
+        quest_account.score,
+        quest_account.deadline,
+        quest_account.max_score
     );
 
     // --- fetch reward before update ---
-    let reward_account: Reward = reward_prog.account(reward_pda)?;
+    let reward_account: Reward = reward.account(reward_pda)?;
     println!(
-        "Reward data - event_id: {}, amount: {}",
-        reward_account.id, reward_account.amount
+        "Reward data - quest_id: {}, amount: {}",
+        reward_account.quest_id, reward_account.amount
     );
 
     // --- manual update reward (direct call) ---
     println!("\n--- Manual Update Reward ---");
-
-    let tx_update = reward_prog
+    // server_admin
+    // system_program
+    // quest_pda
+    // quest_program
+    let tx_update = reward
         .request()
-        .accounts(reward_accounts::UpdateReward {
-            authority: pub_key,
+        .accounts(reward_accounts::ClaimReward {
+            server_admin: admin_pub_key,
+            system_program: anchor_lang::solana_program::system_program::id(),
             reward: reward_pda,
+            quest_pda: quest_pda,
+            quest_program: quest::id(),
         })
-        .args(reward_ix::UpdateReward { event_id })
+        .args(reward_instruction::ClaimReward {
+            quest_id,
+            user_id,
+            server_id,
+            provider_id,
+        })
         .send()?;
 
     println!("UpdateReward tx signature: {}", tx_update);
 
     // --- fetch reward after update ---
-    let reward_after_update: Reward = reward_prog.account(reward_pda)?;
+    let reward_after_update: Reward = reward.account(reward_pda)?;
     println!(
-        "Reward (after manual update) - event_id: {}, amount: {}",
-        reward_after_update.id, reward_after_update.amount
+        "Reward (after manual update) - quest_id: {}, amount: {}",
+        reward_after_update.quest_id, reward_after_update.amount
     );
 
     Ok(())
